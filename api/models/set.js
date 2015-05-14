eb34c8cd2986a6d20b07160bb68a4488b355ff53
gideong <gideong@yahoo-inc.com>
2015-05-13 21:25:56 -0700

    WIP

--- api/models/set.js
+++ api/models/set.js
@@ -1,25 +1,15 @@
 var mongoose = require('mongoose');
-var deepPopulate = require('mongoose-deep-populate');
 
 var factory = null;
 module.exports = function(app) {
 
 	if (factory) return factory;
 
-	var modelName = 'set';
+	var modelName = 'example';
 	var schema = mongoose.Schema({
-		name: String,
-		swing: Number,
-		bpm: Number,
-		pool: [{type: String, ref: 'pool-entry'}],
-		patterns: [{type: String, ref: 'pattern'}],
-		songs: [{type: String, ref: 'song'}]
+		example: String
 	});
 	require('./schema-id')(schema);
-
-	var deepPopulateOptions = {
-	};
-	schema.plugin(deepPopulate, deepPopulateOptions);
 	factory = mongoose.model(modelName, schema);
 
 	return factory;
